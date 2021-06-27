bb0518a5d6f4f4440c8275dbeebbe9fef9269b3b
p-r-i-y-a-n-k-a-533 <priyankakushwaha533@gmail.com>
2021-06-27 17:18:54 +0530

    Engage Teams video calling app

---
+++ public/script.js
@@ -0,0 +1,217 @@
+const socket = io("/");
+const videoBox = document.getElementById("video-box");
+const myVideo = document.createElement("video");
+let textMessage = document.querySelector("#type_here_box");
+let sendButton = document.getElementById("send_button");
+let sentMessages = document.querySelector(".sent_messages_box");
+const inviteButton = document.querySelector("#invite_button");
+const micButton = document.querySelector("#mic_button");
+const videoButton = document.querySelector("#video_button");
+const leaveButton = document.querySelector("#leave_button");
+const busyButton = document.querySelector('#busy_button');
+const video = document.getElementsByTagName('video');
+
+myVideo.muted = true;
+const peersConnected = {}
+const user = prompt("Enter your name for this meet");
+//
+var currentPeer;
+//
+var peer = new Peer(undefined, {
+  path: "/peerjs",
+  host: "/",
+  port: "443"
+});
+
+let my_videoStream;
+navigator.mediaDevices
+  .getUserMedia({
+    audio: true,
+    video: true,
+  })
+  .then((stream) => {
+    my_videoStream = stream;
+    add_videoStream(myVideo, stream);
+
+    peer.on("call", (call) => {
+      call.answer(stream);
+      const video = document.createElement("video");
+      call.on("stream", (user_videoStream) => {
+        add_videoStream(video, user_videoStream);
+        //
+        currentPeer = call.peerConnection
+        //
+      });
+    });
+
+/////////////////////////////////////////////////////////////////////////////
+
+    socket.on("user-connected", (userId) => {
+      setTimeout(()=>{
+        connect_NewUser(userId, stream)
+      },3000)
+      })
+      
+    });
+  
+//////////////////////////////////////////////////////////////////////////////
+
+const connect_NewUser = (userId, stream) => {
+  const call = peer.call(userId, stream);
+  const video = document.createElement("video");
+  call.on("stream", (user_videoStream) => {
+    add_videoStream(video, user_videoStream);
+    //
+    currentPeer=call.peerConnection
+    //
+  });
+  call.on('close',() => {
+    video.remove()
+  })
+  peersConnected[userId] = call
+};
+
+///////////////////////////////////////////////////////////////////////////////
+
+peer.on("open", (id) => {
+  socket.emit("join-room", Room_Id, id, user);
+});
+
+////////////////////////////////////////////////////////////////////////////////
+
+const add_videoStream = (video, stream) => {
+  video.srcObject = stream;
+  video.addEventListener("loadedmetadata", () => {
+    video.play();
+    videoBox.append(video);
+    video.classList.add("border_background")
+  });
+};
+
+///////////////////////////////////////////////////////////////////////////////
+
+sendButton.addEventListener("click", (e) => {
+  if (textMessage.value.length !== 0) {
+    socket.emit("message", textMessage.value);
+    textMessage.value = "";
+  }
+});
+
+///////////////////////////////////////////////////////////////////////////////
+
+textMessage.addEventListener("keydown", (e) => {
+  if (e.key === "Enter" && textMessage.value.length !== 0) {
+    socket.emit("message", textMessage.value);
+    textMessage.value = "";
+  }
+});
+
+/////////////////////////////////////////////////////////////////////////////////
+
+micButton.addEventListener("click", () => {
+  const enabled = my_videoStream.getAudioTracks()[0].enabled;
+  if (enabled) {
+    my_videoStream.getAudioTracks()[0].enabled = false;
+    html = `<i class="fas fa-microphone-slash"></i>`;
+    micButton.classList.toggle("background__red");
+    micButton.innerHTML = html;
+  } else {
+    my_videoStream.getAudioTracks()[0].enabled = true;
+    html = `<i class="fas fa-microphone"></i>`;
+    micButton.classList.toggle("background__red");
+    micButton.innerHTML = html;
+  }
+});
+
+///////////////////////////////////////////////////////////////////////////
+
+videoButton.addEventListener("click", () => {
+  const enabled = my_videoStream.getVideoTracks()[0].enabled;
+  if (enabled) {
+    my_videoStream.getVideoTracks()[0].enabled = false;
+    html = `<i class="fas fa-video-slash"></i>`;
+    videoButton.classList.toggle("background__red");
+    videoButton.innerHTML = html;
+  }
+  else {
+    my_videoStream.getVideoTracks()[0].enabled = true;
+    html = `<i class="fas fa-video"></i>`;
+    videoButton.classList.toggle("background__red");
+    videoButton.innerHTML = html;
+  }
+});
+
+//////////////////////////////////////////////////////////////////////
+
+busyButton.addEventListener("click", () => {
+  busyButton.classList.toggle("background__orange");
+   {
+     
+    
+         navigator.mediaDevices.getDisplayMedia( {
+            video: {
+                cursor: "none"
+                
+            },
+            audio: {
+                echoCancellation: true,
+                noiseSuppression: true,
+            }
+        } ).then((stream) => {
+          let videoTrack = stream.getVideoTracks()[0];
+          videoTrack.onended = function(){
+            stopScreenShare();
+          }
+          let sender = currentPeer.getSenders().find(function(s){
+            return s.track.kind == videoTrack.kind;
+          })
+          sender.replaceTrack(videoTrack)
+        }).catch((err)=>{
+             console.log("------------------"+err)
+        })
+    }
+    
+   
+
+  });
+
+  function stopScreenShare(){
+    let videoTrack = my_videoStream.getVideoTracks()[0];
+    var sender = currentPeer.getSenders().find(function(s){
+      return s.track.kind == videoTrack.kind;
+    })  
+    sender.replaceTrack(videoTrack)
+    busyButton.classList.toggle("background__orange");
+  }
+
+/////////////////////////////////////////////////////////////////////////////
+
+inviteButton.addEventListener("click", (e) => {
+  prompt(
+    "Copy this link and send it to people you want to add",
+    window.location.href
+  );
+});
+
+/////////////////////////////////////////////////////////////////////////////
+
+leaveButton.addEventListener("click",() => {
+  const enabled = my_videoStream.getVideoTracks()[0].enabled;
+  if (enabled) {
+    my_videoStream.getVideoTracks()[0].enabled = false;}
+  document.getElementsByTagName ('html') [0] .remove ();
+});
+
+/////////////////////////////////////////////////////////////////////////////
+
+socket.on("createMessage", (message, userName) => {
+  sentMessages.innerHTML =
+  sentMessages.innerHTML+
+    `<div class="message">
+        <b><i class="far fa-user-circle"></i> <span> ${
+          userName === user ? "me" : userName
+        }</span> </b>
+        <span>${message}</span>
+    </div>`;
+});
+
---
+++ server.js
@@ -0,0 +1,96 @@
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+const request = require("request");
+const  bodyParser = require("body-parser");
+const https = require("https");
+
+/////////////////////////////////////////////////////////////////////////////
+
+app.set("view engine", "ejs");
+
+const io = require("socket.io")(server, {
+  cors: {
+    origin: '*'
+  }
+});
+
+////////////////////////////////////////////////////////////////////////////
+
+const { ExpressPeerServer } = require("peer");
+const peerServer = ExpressPeerServer(server, {
+  debug: true,
+});
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use("/peerjs", peerServer);
+app.use(express.static("public"));
+
+/////////////////////////////////////////////////////////////////////////////
+
+app.get("/",function(req,res)
+{
+  res.sendFile(__dirname+"/views/index.html");
+});
+
+////////////////////////////////////////////////////////////////////////////
+
+app.get("/:room", (req, res) => {
+  res.render("room", { roomId: req.params.room });
+});
+
+////////////////////////////////////////////////////////////////////////////
+
+app.post("/",function(req,res)
+{
+  const name = req.body.first;
+  const sirname = req.body.last;
+  const email = req.body.e;
+  const data = {
+    members:[{ email_address: email,
+               status: "subscribed",
+               merge_fields:{FNAME: name,
+                             LNAME: sirname}
+            }]
+  };
+
+  const jsonData = JSON.stringify(data);
+  const url = "https://us10.api.mailchimp.com/3.0/lists/dd67644160";
+  const options = { method: "POST",
+                    auth:"pr1:8f71e0a6f1a402f8333273a965d6b86d-us10"
+                  }
+  const request = https.request(url,options,function(response)
+  {  if(res.statusCode === 200)
+      { //res.sendFile(__dirname+"/views/room.ejs");
+        res.render("room", { roomId: req.params.room });}
+  }
+
+////////////////////////////////////////////////////////////////////////////
+
+    ,res.on("data",function(data)
+      {    console.log(JSON.parse(data));
+      })
+    );
+  request.write(jsonData);
+  request.end();
+
+});
+
+///////////////////////////////////////////////////////////////////////////
+
+io.on("connection", (socket) => {
+  socket.on("join-room", (roomId, userId, userName) => {
+    socket.join(roomId);
+    socket.to(roomId).emit("user-connected", userId);
+    socket.on("message", (message) => {
+      io.to(roomId).emit("createMessage", message, userName);
+    });
+    socket.on('disconnect', (userId) => {
+      socket.to(roomId).emit('user-disconnected', userId)
+    });
+  });
+});
+
+///////////////////////////////////////////////////////////////////////////
+
+server.listen(process.env.PORT || 3030);
